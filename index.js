48562b842aef376673e0be8b11e7a33b43005fdd
Joao Pedro <jotappe.domingues05@gmail.com>
2023-04-26 21:20:47 -0300

    Corrigindo exercicios

---
+++ index.js
@@ -0,0 +1,66 @@
+/*
+function mensagem(texto){
+	return texto.toUpperCase() + ' - ' + texto.length;
+}
+ 
+
+ console.log(mensagem('string'));
+ //b)
+
+ function soma(){
+	return n1+ n2;
+
+ }
+
+ function subtração(){
+	return n1 - n2;
+	
+ }
+
+ function multiplicação(){
+	return n1 * n2;
+	
+ }
+
+ function divisão(){
+	return n1 / n2;
+	
+ }
+
+ let somaDois = soma(2, 3);
+ let subDois = subtração(2, 3);
+ let multiDois = multiplicação(2, 3);
+ let divDois = divisão(2, 3);
+
+ console.log('Numeros inseridos:  '+2+ ' , '+3);
+ console.log('soma:' + somaDois);
+ console.log('Diferença:' + subDois);
+ console.log('divisão:'+ divDois )
+ console.log('Multiplicação:' + multiDois);
+
+ //  hip² = a² + b²
+/*
+ function hipo (cat1, cat2){
+	return Math.sqrt(Math.pow(cat1,2) +(cat2, 2))	
+ }
+ console.log(hipo(3,4).toFixed(2));
+
+ function imprimir(){
+
+	return "Eu sou o Joao, tenho 15 anos, moro em Rio Grande do Sul e sou Estudante"
+ }
+
+ console.log(imprimir);
+
+ function informacoesPessoa( nome, idade, cidade, profissão){
+	console.log(nome);
+	return   olá ${nome}, tenho ${idade} 
+ }
+
+
+ /*let nomePessoa = prompt('Nome:');
+ let idadePessoa = Number(prompt('Idade:'));
+ let pro
+ alert(informacoesPessoa(parametro1, nomePessoa, 'São Leopoldo , estudante'));
+ 
+
